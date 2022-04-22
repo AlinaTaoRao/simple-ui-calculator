91086fe656c5a37f6ff6f70a93e02f73013daa9f
AlinaTaoRao <raotao77@outlook.com>
2022-04-22 20:28:18 +0200

    small change

--- simple-ui/src/App.js
+++ simple-ui/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { CalculatorContainer } from './components/CalculatorContainer.jsx'; //for way 1 and way 2
 
+
+/* for way 1 and way 2, works */
 function App() {
   return (
    <header>
