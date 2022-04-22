49cc30bb1dc64a883aab2f63613084beeee5bd12
AlinaTaoRao <raotao77@outlook.com>
2022-04-22 20:27:27 +0200

    way 1 and way 2

--- simple-ui/src/App.js
+++ simple-ui/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { CalculatorContainer } from './components/CalculatorContainer.jsx';
+import { CalculatorContainer } from './components/CalculatorContainer.jsx'; //for way 1 and way 2
 
 function App() {
   return (
--- simple-ui/src/components/CalculatorContainer.jsx
+++ simple-ui/src/components/CalculatorContainer.jsx
@@ -5,6 +5,7 @@ import { CalcItem3 } from "./calc-item-3/CalcItem3.jsx";
 import { CalcItem4 } from "./calc-item-4/CalcItem4.jsx";
 import { CalcItem5 } from "./calc-item-5/CalcItem5.jsx";
 
+/* way 1, for way1 and way 2-{} expression, works */
 export const CalculatorContainer = () => {
   return (
     <div className="calculator-container">
@@ -21,3 +22,26 @@ export const CalculatorContainer = () => {
     </div>
   );
 };
+
+
+/* way 3: for props , not work */
+// const myType = "submit";
+// const myValues = ["AC", "DEL", "%"];
+// export const CalculatorContainer = () => {
+//   return (
+//     <div className="calculator-container">
+//       <input
+//         className="display-header"
+//         type="text"
+//         placeholder="468 % 2 = 234"
+//       />
+//       {myValues.map((myValue, index) => (<CalcItem1 key = {index} type={myType} value={myValue} />))}
+      
+//       <CalcItem2 />
+//       <CalcItem3 />
+//       <CalcItem4 />
+//       <CalcItem5 />
+//     </div>
+//   );
+// };
+
